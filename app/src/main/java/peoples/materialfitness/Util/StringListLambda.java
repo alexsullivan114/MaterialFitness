4f54719a8e79045e11789b29edb71c0bfb292b31
Alex <alex.sullivan@runkeeper.com>
2015-10-22 07:25:22 -0400

    Comments.

--- app/src/main/java/peoples/materialfitness/Util/StringListLambda.java
+++ app/src/main/java/peoples/materialfitness/Util/StringListLambda.java
@@ -5,6 +5,10 @@ import java.util.List;
 /**
  * Created by Alex Sullivan on 10/18/2015.
  */
+
+/**
+ * Simple lambda interface to use a list of strings.
+ */
 public interface StringListLambda
 {
     void apply(List<String> values);
