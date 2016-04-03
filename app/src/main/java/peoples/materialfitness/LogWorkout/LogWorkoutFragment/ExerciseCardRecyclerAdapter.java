a22ce7691aa5a7eb005a078079621a8d76421b77
Alex Sullivan <Alex.sullivan@runkeeper.com>
2016-04-03 17:28:33 -0400

    Fixed up the dropdown animation! Should be good to go.

--- app/src/main/java/peoples/materialfitness/LogWorkout/LogWorkoutFragment/ExerciseCardRecyclerAdapter.java
+++ app/src/main/java/peoples/materialfitness/LogWorkout/LogWorkoutFragment/ExerciseCardRecyclerAdapter.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -29,7 +28,7 @@ public class ExerciseCardRecyclerAdapter extends RecyclerView.Adapter<ExerciseCa
 {
     private WorkoutSession mWorkoutSession;
     private ExerciseCardAdapterInterface mCallback;
-    private static final int EXPAND_ANIMATION_DURATION = 600;
+    private static final int SHOW_SPILLOVER_ANIMATION_DURATION = 600;
 
     private static final int NUM_DISPLAY_SETS = 5;
 
@@ -62,9 +61,18 @@ public class ExerciseCardRecyclerAdapter extends RecyclerView.Adapter<ExerciseCa
         // Now loop through our weights and add each one to our container.
         for (int i = 0; i < exerciseSession.getSets().size(); i++)
         {
+            // Show our divider view so the user can drop down to reveal more sets.
+            // +1 for zeri indexing offsets :/
+            if (i+1 > NUM_DISPLAY_SETS)
+            {
+                holder.dropDownLayout.setVisibility(View.VISIBLE);
+            }
+
             WeightSet weightSet = exerciseSession.getSets().get(i);
             addWeightSetViewToHolder(holder, weightSet);
-            // Lets only do this once for performance.
+            // Lets only do this once for performance. Lock the linear layout to the current height,
+            // since we want to include the extra items but we don't want the layout to grow with them.
+            // We will grow the layout via animation if the user selects the dropdown.
             // +1 for zero indexing offset.
             if (i+1 == NUM_DISPLAY_SETS)
             {
@@ -94,6 +102,11 @@ public class ExerciseCardRecyclerAdapter extends RecyclerView.Adapter<ExerciseCa
         notifyItemInserted(mWorkoutSession.getExercises().size() - 1);
     }
 
+    /**
+     * Add a weight set view to the view holder. Convenience method to add a set view to the container.
+     * @param holder Holder to add to.
+     * @param weightSet The weight set whos view we're constructing.
+     */
     private void addWeightSetViewToHolder(ExerciseCardViewHolder holder, WeightSet weightSet)
     {
         LayoutInflater inflater = LayoutInflater.from(holder.mCardView.getContext());
@@ -111,6 +124,80 @@ public class ExerciseCardRecyclerAdapter extends RecyclerView.Adapter<ExerciseCa
 
         holder.mRepContainer.addView(setContainer);
     }
+
+    private void onDropdownClicked(ExerciseCardViewHolder viewHolder)
+    {
+        animateSpilloverSetsToggle(viewHolder, !viewHolder.isDropdownToggled);
+        viewHolder.isDropdownToggled = !viewHolder.isDropdownToggled;
+    }
+
+    /**
+     * Animate showing or hiding our spillover sets. We do two basic animations here - animating the
+     * height of the rep container and doing a rotation animation on the dropdown indicator.
+     * @param viewHolder Viewholder to animate
+     * @param shouldExpand Boolean telling us if we're showing the spillover sets or hiding them.
+     */
+    private void animateSpilloverSetsToggle(ExerciseCardViewHolder viewHolder, boolean shouldExpand)
+    {
+        ExerciseSession exerciseSession = mWorkoutSession.getExercises().get(viewHolder.getAdapterPosition());
+        int spilloverSetsNum = exerciseSession.getSets().size() - NUM_DISPLAY_SETS;
+        // Note: We know we have at least NUM_DISPLAY_SETS children here, which is why this is safe.
+        // TODO: However, we're not really sure that the items in the recyclerview are this height.
+        // They are right now but I'm sure that'll change.
+        View firstChild = viewHolder.mRepContainer.getChildAt(0);
+        ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams)firstChild.getLayoutParams();
+
+        int totalHeight = firstChild.getHeight() + layoutParams.bottomMargin + layoutParams.topMargin + firstChild.getPaddingBottom();
+        int heightOfSpilloverSets = totalHeight * spilloverSetsNum;
+        int heightOfContainer = viewHolder.mRepContainer.getHeight();
+
+        int desiredHeight = 0;
+        float startRotation = 0;
+        float endRotation = 0;
+
+        if (shouldExpand)
+        {
+            desiredHeight = heightOfContainer + heightOfSpilloverSets;
+            startRotation = 0f;
+            endRotation = 180f;
+        }
+        else
+        {
+            desiredHeight = heightOfContainer - heightOfSpilloverSets;
+            startRotation = 180f;
+            endRotation = 360f;
+        }
+
+        ObjectAnimator rotationAnimator = ObjectAnimator.ofFloat(viewHolder.dividerView, "rotation", startRotation, endRotation);
+        HeightAnimator heightAnimator = new HeightAnimator(viewHolder.mRepContainer, desiredHeight);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(heightAnimator, rotationAnimator);
+        animatorSet.setDuration(SHOW_SPILLOVER_ANIMATION_DURATION);
+        animatorSet.setInterpolator(new FastOutSlowInInterpolator());
+        animatorSet.start();
+    }
+
+    private void onCardClicked(ExerciseCardViewHolder viewHolder)
+    {
+        ExerciseSession exerciseSession = mWorkoutSession.getExercises().get(viewHolder.getAdapterPosition());
+        mCallback.onExerciseClicked(exerciseSession);
+    }
+
+    /**
+     * Interface for communication between this adapter and the calling/creating class.
+     */
+    public interface ExerciseCardAdapterInterface
+    {
+        void onExerciseClicked(ExerciseSession session);
+    }
+
+    public void setWorkoutSession(WorkoutSession workoutSession)
+    {
+        mWorkoutSession = workoutSession;
+        notifyDataSetChanged();
+    }
+
 
     protected class ExerciseCardViewHolder extends RecyclerView.ViewHolder
     {
@@ -143,99 +230,5 @@ public class ExerciseCardRecyclerAdapter extends RecyclerView.Adapter<ExerciseCa
         {
             ExerciseCardRecyclerAdapter.this.onDropdownClicked(this);
         }
-    }
-
-    private void onDropdownClicked(ExerciseCardViewHolder viewHolder)
-    {
-        boolean isDropdownToggled = viewHolder.isDropdownToggled;
-
-        if (isDropdownToggled)
-        {
-            removeWeightSets(viewHolder);
-        }
-        else
-        {
-            addWeightSets(viewHolder);
-        }
-
-        viewHolder.isDropdownToggled = !viewHolder.isDropdownToggled;
-    }
-
-    private void addWeightSets(ExerciseCardViewHolder viewHolder)
-    {
-        ExerciseSession exerciseSession = mWorkoutSession.getExercises().get(viewHolder.getAdapterPosition());
-        int spilloverSetsNum = exerciseSession.getSets().size() - NUM_DISPLAY_SETS;
-        // Note: We know we have at least NUM_DISPLAY_SETS children here, which is why this is safe.
-        View firstChild = viewHolder.mRepContainer.getChildAt(0);
-        ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams)firstChild.getLayoutParams();
-        int totalheight = firstChild.getHeight() + layoutParams.bottomMargin + layoutParams.topMargin + firstChild.getPaddingBottom();
-        int heightOfSetsToAdd = totalheight * spilloverSetsNum;
-        int heightOfContainer = viewHolder.mRepContainer.getHeight();
-
-        ObjectAnimator rotationAnimator = ObjectAnimator.ofFloat(viewHolder.dividerView, "rotation", 0f, 180f);
-        HeightAnimator heightAnimator = new HeightAnimator(viewHolder.mRepContainer, heightOfContainer + heightOfSetsToAdd);
-
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(heightAnimator, rotationAnimator);
-        animatorSet.setDuration(EXPAND_ANIMATION_DURATION);
-        animatorSet.setInterpolator(new FastOutSlowInInterpolator());
-        animatorSet.start();
-    }
-
-    private void removeWeightSets(ExerciseCardViewHolder viewHolder)
-    {
-        ExerciseSession exerciseSession = mWorkoutSession.getExercises().get(viewHolder.getAdapterPosition());
-        int spilloverSetsNum = exerciseSession.getSets().size() - NUM_DISPLAY_SETS;
-        // Note: We know we have at least NUM_DISPLAY_SETS children here, which is why this is safe.
-        View firstChild = viewHolder.mRepContainer.getChildAt(0);
-        ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams)firstChild.getLayoutParams();
-        int totalheight = firstChild.getHeight() + layoutParams.bottomMargin + layoutParams.topMargin + firstChild.getPaddingBottom();
-        int heightOfSetsToRemove = totalheight * spilloverSetsNum;
-        int heightOfContainer = viewHolder.mRepContainer.getHeight();
-        int desiredHeight = heightOfContainer - heightOfSetsToRemove;
-
-        ObjectAnimator rotationAnimator = ObjectAnimator.ofFloat(viewHolder.dividerView, "rotation", 180f, 360f);
-        HeightAnimator heightAnimator = new HeightAnimator(viewHolder.mRepContainer, desiredHeight);
-
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(heightAnimator, rotationAnimator);
-        animatorSet.setDuration(EXPAND_ANIMATION_DURATION);
-        animatorSet.setInterpolator(new FastOutSlowInInterpolator());
-        animatorSet.start();
-    }
-
-    private void animateDividerView(boolean isDropdownToggled, ImageView dividerView)
-    {
-
-        int pivotX = dividerView.getWidth()/2;
-        int pivotY = dividerView.getHeight()/2;
-        int startDegrees = isDropdownToggled ? 180 : 0;
-        int endDegrees = isDropdownToggled ? 360 : 180;
-
-        RotateAnimation rotateAnimation = new RotateAnimation(startDegrees, endDegrees, pivotX, pivotY);
-        rotateAnimation.setInterpolator(new FastOutSlowInInterpolator());
-        rotateAnimation.setDuration(500);
-        rotateAnimation.setFillAfter(true);
-        dividerView.startAnimation(rotateAnimation);
-    }
-
-    private void onCardClicked(ExerciseCardViewHolder viewHolder)
-    {
-        ExerciseSession exerciseSession = mWorkoutSession.getExercises().get(viewHolder.getAdapterPosition());
-        mCallback.onExerciseClicked(exerciseSession);
-    }
-
-    /**
-     * Interface for communication between this adapter and the calling/creating class.
-     */
-    public interface ExerciseCardAdapterInterface
-    {
-        void onExerciseClicked(ExerciseSession session);
-    }
-
-    public void setWorkoutSession(WorkoutSession workoutSession)
-    {
-        mWorkoutSession = workoutSession;
-        notifyDataSetChanged();
     }
 }
